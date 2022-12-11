3d5807db38c547e56d70d410ebba1b43f06566df
Alex Arador Triano <alex.arador@gmail.com>
2022-12-11 15:14:43 +0100

    Deleted Darwinex API integration files

--- darwinexapi/common.py
+++
@@ -1,3 +0,0 @@
-def not_successful(status_code, response_body):
-    print('There was an error processing your request. Error code: {}. Description: {}'.format(status_code, response_body))
-    return None
